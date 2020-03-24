5590c5e3b9d53a1cec8860f929d20eca6cdba148
mmrota <mmrota@debian>
2020-03-24 20:56:27 +0100

    Added neighbor discovery to FADIA

--- Fadia/common.h
+++ Fadia/common.h
@@ -19,12 +19,14 @@ using namespace omnetpp;
 #define NOID 0
 #define MAXUID INT32_MAX
 
-typedef uint32_t UID;               // Unique ID of a device
-typedef uint32_t KEYID;             // ID of a key
-typedef uint32_t KEY;               // A key (We suppose it is SHA256 for now... can be changed later)
-typedef std::map<KEYID, KEY> KEYRNG;     // A KeyRing, pair of Key ID and Key.
+typedef uint32_t UID;                                       // Unique ID of a device
+typedef uint32_t KEYID;                                     // ID of a key
+typedef uint32_t KEY;                                       // A key (We suppose it is SHA256 for now... can be changed later)
+typedef std::map<KEYID, KEY> KEYRNG;                        // A KeyRing, pair of Key ID and Key.
+typedef std::map<KEYID, KEY>::iterator KEYRNG_IT;           // Iterator for KEYRNG.
 
 
-const KEYRNG getKeyPool(cRNG* rng, size_t KPsize);
 
+KEYRNG getKeyRing(cRNG* rng, size_t KPsize, size_t KRsize);
+const UID getBaseID();
 #endif /* COMMON_H_ */
